0ae082dc961d499f34cd84c4421b1c37b27fbeb9
yem1546 <87500366+yem1546@users.noreply.github.com>
2022-09-12 20:32:17 -0400

    completed mvp

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,60 +1,67 @@
-/*
-  STEP 1: using axios, send a GET request to the following URL
-    (replacing the placeholder with your Github name):
-    https://api.github.com/users/<your name>
-*/
+import axios from "axios";
 
-/*
-  STEP 2: Inspect and study the data coming back, this is YOUR
-    github info! You will need to understand the structure of this
-    data in order to use it to build your component function
+const entryPoint = document.querySelector(".cards")
+const followersArray = ["tetondan",
+  "dustinmyers",
+  "justsml",
+  "luishrd",
+  "bigknell"];
 
-    Skip to STEP 3 (line 34).
-*/
+function userData(user){
+  const url = `https://api.github.com/users/${user}`
+  axios.get(url)
+  .then(rep => {
+    console.log(rep.data)
+    console.log(cardMaker(rep.data));
+    entryPoint.appendChild(cardMaker(rep.data))
+  })
+  .catch(err => {
+    console.log(err);
+  })
+  .finally()
+}
 
-/*
-  STEP 4: Pass the data received from Github into your function,
-    and append the returned markup to the DOM as a child of .cards
-*/
+function cardMaker(dataObj){
+  const card = document.createElement("div")
+  const image = document.createElement("img")
+  const info = document.createElement("div")
+  const realName = document.createElement("h3")
+  const userName = document.createElement("p")
+  const loc = document.createElement("p")
+  const profile = document.createElement("p")
+  const link = document.createElement("a")
+  const followers = document.createElement("p")
+  const following = document.createElement("p")
+  const bio = document.createElement("p")
+  
+  card.classList.add("card")
+  info.classList.add("card-info")
+  realName.classList.add("name")
+  userName.classList.add("username")
+  
+  image.src = dataObj.avatar_url
+  realName.textContent = dataObj.name
+  userName.textContent = dataObj.login
+  loc.textContent = `Location: ${dataObj.location}`
+  profile.textContent = "Profile: "
+  link.href = dataObj.html_url
+  link.textContent = dataObj.html_url
+  followers.textContent = `Followers: ${dataObj.followers}`
+  following.textContent = `Following: ${dataObj.following}`
+  bio.textContent = `Bio: ${dataObj.bio}`
 
-/*
-  STEP 5: Now that you have your own card getting added to the DOM, either
-    follow this link in your browser https://api.github.com/users/<Your github name>/followers,
-    manually find some other users' github handles, or use the list found at the
-    bottom of the page. Get at least 5 different Github usernames and add them as
-    Individual strings to the friendsArray below.
+  card.appendChild(image)
+  card.appendChild(info)
+  info.appendChild(realName)
+  info.appendChild(userName)
+  info.appendChild(loc)
+  info.appendChild(profile)
+  profile.appendChild(link)
+  info.appendChild(followers)
+  info.appendChild(following)
+  info.appendChild(bio)
+  return card
+}
 
-    Using that array, iterate over it, requesting data for each user, creating a new card for each
-    user, and adding that card to the DOM.
-*/
-
-const followersArray = [];
-
-/*
-  STEP 3: Create a function that accepts a single object as its only argument.
-    Using DOM methods and properties, create and return the following markup:
-
-    <div class="card">
-      <img src={image url of user} />
-      <div class="card-info">
-        <h3 class="name">{users name}</h3>
-        <p class="username">{users user name}</p>
-        <p>Location: {users location}</p>
-        <p>Profile:
-          <a href={address to users github page}>{address to users github page}</a>
-        </p>
-        <p>Followers: {users followers count}</p>
-        <p>Following: {users following count}</p>
-        <p>Bio: {users bio}</p>
-      </div>
-    </div>
-*/
-
-/*
-  List of LS Instructors Github username's:
-    tetondan
-    dustinmyers
-    justsml
-    luishrd
-    bigknell
-*/
+userData("yem1546")
+followersArray.forEach(user => userData(user))
